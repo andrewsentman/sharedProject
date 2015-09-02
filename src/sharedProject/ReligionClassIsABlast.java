c8c62ba9af907cb8c8772dfed66206a740d33cc1
TheSeveralBubbles <pj.brady@comcast.net>
2015-09-01 17:56:34 -0700

    I am making another class as part of a test!

---
+++ src/sharedProject/ReligionClassIsABlast.java
@@ -0,0 +1,7 @@
+package sharedProject;
+
+public class ReligionClassIsABlast {
+	public static void main(String args[]){
+		System.out.println("I can do this very fast but this is a very bad way of typing.");
+	}
+}
